c633934ee3887ec1c1f9555fa7b4b5f1b62dc5ef
VforVitorio <victorvegasobral@yahoo.com>
2025-10-20 15:35:31 +0200

    changed files to connect fastapi backend with streamlit frontend. - NOTE: to run with docker or without it we need to create env variables and import BAKCEND_URL from the config file

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,12 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="F1 Telemetry API")
 
 
+# NOTE: we will need to change the allow_origins route to use env variables in config.py file
+# with this we could either run it with Docker or without it
+# import os
+
+# BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:8501"],
--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import requests
+# from config impor BACKEND_URL
 
 st.set_page_config(
     page_title="F1 Telemetry Manager",
@@ -8,3 +10,17 @@ st.set_page_config(
 
 st.title("🏎️ F1 Telemetry Manager")
 st.write("Welcome! The setup is working.")
+
+# try backend connection
+if st.button("Test Backend Connection"):
+    try:
+        # NOTE: need to change this when config file is made
+        # response = requests.get(f"{BACKEND_URL}")
+        response = requests.get("http://localhost:8000")
+        if response.status_code == 200:
+            st.success(
+                f"✅ Connected! Backend says: {response.json()['message']}")
+        else:
+            st.error(f"❌ Error: {response.status_code}")
+    except Exception as e:
+        st.error(f"❌ Cannot reach backend: {str(e)}")
