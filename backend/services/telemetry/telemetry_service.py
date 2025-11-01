5a547756e21c4ee8c2e13028c802f498fea363a2
Santiago <149386030+Santisoutoo@users.noreply.github.com>
2025-11-01 14:44:35 +0100

    initial call to fastf1 api with the parameters selected

--- backend/services/telemetry/telemetry_service.py
+++ backend/services/telemetry/telemetry_service.py
@@ -1,11 +1,29 @@
+from .fastf1_client import SessionData
+import pandas as pd
 
 def get_telemetry_data_from_db(year: int, gp: str, session: str, drivers: list):
     print(f"Backend recibió -> Year: {year}, GP: {gp}, Session: {session}, Drivers: {drivers}")
+    
+    session_data = SessionData(
+        year=year,
+        circuit=gp,
+        current_session=session,
+        drivers=drivers
+    )
+    
+    laps_data = session_data.get_driver_lap_times()
+    
+    print(laps_data)
+    
+    # Reemplazar NaN y valores infinitos antes de serializar
+    if not laps_data.empty:
+        laps_data = laps_data.fillna(0)
+        laps_data = laps_data.replace([float('inf'), float('-inf')], 0)
+    
     return {
         "year": year,
-        "gp": gp, 
+        "gp": gp,
         "session": session,
         "drivers": drivers,
-        "telemetry": []
+        "laps": laps_data.to_dict('records') if not laps_data.empty else []
     }
-
